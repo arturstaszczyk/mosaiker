a01c809b88f75e8cbab1ca39c5b59ed3c5be3fb3
Artur Staszczyk <astaszczyk@ganymede.eu>
2016-05-20 17:56:36 +0200

    CommandMakeMosaik mocked

--- MosaikerLib/Commands/CommandCreateMosaic.cpp
+++ MosaikerLib/Commands/CommandCreateMosaic.cpp
@@ -1,4 +1,9 @@
-#include "CommandCreateMosaic.h"
+﻿#include "CommandCreateMosaic.h"
+
+#include <QThread>
+#include <QDebug>
+
+#include "ImageIndexer.h"
 
 CommandCreateMosaic::CommandCreateMosaic(QObject* parent)
     : Command(COMMAND_NAME(CommandCreateMosaic), parent)
@@ -8,5 +13,24 @@ CommandCreateMosaic::CommandCreateMosaic(QObject* parent)
 
 void CommandCreateMosaic::execute()
 {
-    CommandFinisher finisher(this);
+    //mIndexLoader->loadIndex();
+    //auto image = mMainImageModel->image();
+
+    //auto quads = mImageSlicer->slice(image, QSize(64, 64));
+
+    QThread* indexer = new ImageIndexer({}, this);
+    connect(indexer, SIGNAL(imageIndexed(QString, quint32)),
+                     this, SLOT(onImageIndexed(QString, quint32)));
+    connect(indexer, SIGNAL(finished()), this, SLOT(finished()));
+    indexer->start();
+}
+
+void CommandCreateMosaic::finished()
+{
+    finish();
+}
+
+void CommandCreateMosaic::onImageIndexed(QString name, quint32 color)
+{
+    qDebug() << color;
 }
--- MosaikerLib/Commands/CommandCreateMosaic.h
+++ MosaikerLib/Commands/CommandCreateMosaic.h
@@ -1,13 +1,19 @@
-#ifndef COMMANDCREATEMOSAIC_H
+﻿#ifndef COMMANDCREATEMOSAIC_H
 #define COMMANDCREATEMOSAIC_H
 
 #include "Command.h"
 class CommandCreateMosaic : public Command
 {
+    Q_OBJECT
+
 public:
     explicit CommandCreateMosaic(QObject* parent = nullptr);
 
     virtual void execute();
+
+public slots:
+    void onImageIndexed(QString, quint32);
+    void finished();
 };
 
 #endif // COMMANDCREATEMOSAIC_H
